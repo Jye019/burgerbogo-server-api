f9a3ea177bd022f751a4dfcc48a1d798a3d195ce
Jye019 <jihyekim019@gmail.com>
2020-07-20 17:28:39 +0900

    회원가입 페이지 router 생성

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 import express from "express";
+import join from "./routes/join";
 
 const app = express();
 
 app.get("/", (req, res, next) => {
   res.send("success!");
-  
 });
+app.use('/join', join);
 
 app.listen(3000, () => {
   console.log(`Listening at port 3000`);
---
+++ routes/join.js
@@ -0,0 +1,9 @@
+import express from "express";
+ 
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send("회원가입 페이지");
+});
+
+export default router;
