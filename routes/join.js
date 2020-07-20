18134d7cda9a3e3cda9258c0af4bdcda730c6259
Jye019 <jihyekim019@gmail.com>
2020-07-20 17:37:54 +0900

    git any branch test

--- routes/join.js
+++ routes/join.js
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send("회원가입 페이지");
+    res.send("회원가입 페이지!!!");
 });
 
 export default router;
