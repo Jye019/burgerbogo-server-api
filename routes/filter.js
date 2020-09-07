22b42ec6bf61a06b08cc69bc19c9123a7fc79601
Jye019 <jihyekim019@gmail.com>
2020-09-08 02:03:28 +0900

    Feat: 필터 기능 추가

--- routes/filter.js
+++ routes/filter.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.get('/', async(req, res) => {
     try {
         const list =  await db.sequelize.query(
-                        `SELECT image, 
+                        `SELECT DISTINCT image, 
                                 score, 
                                 score_cnt,
                                 price_single, 
@@ -16,23 +16,23 @@ router.get('/', async(req, res) => {
                                 burger.id as id,
                                 burger.name as name, 
                                 brand.name as brand_name
-                         FROM burgers AS burger
-                         LEFT JOIN (SELECT burger_id, id FROM burgers_have_ingredients WHERE ingredient_id IN (${req.body.allergy})) bi
-                         ON burger.id = bi.burger_id 
-                         INNER JOIN (SELECT burger_id, id FROM burgers_have_ingredients WHERE ingredient_id IN (${req.body.main})) bi2 
-                         ON burger.id = bi2.burger_id
-                         INNER JOIN brands AS brand 
-                         ON brand.id = burger.brand_id
-                         LEFT JOIN (
+                        FROM burgers AS burger
+                        LEFT JOIN (SELECT burger_id, id FROM burgers_have_ingredients WHERE ingredient_id IN (${req.body.allergy})) bi
+                        ON burger.id = bi.burger_id 
+                        INNER JOIN (SELECT burger_id, id FROM burgers_have_ingredients WHERE ingredient_id IN (${req.body.main})) bi2 
+                        ON burger.id = bi2.burger_id
+                        INNER JOIN brands AS brand 
+                        ON brand.id = burger.brand_id
+                        LEFT JOIN (
                             SELECT ROUND(AVG(SCORE), 1) AS score,
                                     COUNT(burger_id) AS score_cnt,
                                     burger_id 
                             FROM reviews 
                             GROUP BY burger_id) AS review 
-                         ON review.burger_id=burger.id
-                         WHERE bi.id IS NULL
-                         AND score_cnt >= ${(req.body.order==='score')? 3: 0}
-                         ORDER BY ${req.body.order} ${(req.body.order==='name'||req.body.order==='calorie')? 'ASC' : 'DESC'}`, 
+                        ON review.burger_id=burger.id
+                        WHERE COALESCE(score_cnt, 0) >= ${(req.body.order==='score')? 3: 0}
+                        AND bi.id IS NULL
+                        ORDER BY ${req.body.order} ${(req.body.order==='name'||req.body.order==='calorie')? 'ASC' : 'DESC'}`,
                         { 
                             type: QueryTypes.SELECT,
                             nest: true,
