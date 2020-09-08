c9bef2109d5ab442bd71d009e738d415cfdc4def
minxd95 <newobj95@gmail.com>
2020-09-08 17:50:46 +0900

    Ing: 쿼리파람 형식 수정

--- library/parsing.js
+++ library/parsing.js
@@ -6,28 +6,40 @@ const parseQueryString = (res, _query, curModel, incModels) => {
   const query = _query;
   let includeTmp = [];
   let attributeTmp = [];
+  let whereObjTmp = {};
   let whereTmp = [];
   let order = [];
   let limit = 0;
   let offset = 0;
-  if (query._where) {
-    whereTmp = query._where.split(",");
-  }
   if (query._include) {
     includeTmp = query._include.split(",");
+    delete query._include;
   }
   if (query._attribute) {
     attributeTmp = query._attribute.split(",");
+    delete query._attribute;
   }
   if (query._order) {
     order.push(query._order.split(" "));
+    delete query._order;
   } else order = null;
   if (query._limit) {
     limit = query._limit * 1;
+    delete query._limit;
   } else limit = null;
   if (query._offset) {
     offset = limit * (query._offset * 1 - 1);
+    delete query._offset;
   } else offset = null;
+
+  if (query) {
+    whereObjTmp = query;
+    for (const [key, value] of Object.entries(whereObjTmp)) {
+      const split = key.split(".");
+      whereTmp.push(split.concat([value]));
+    }
+    console.log(whereTmp);
+  }
 
   /* -------------------------*/
 
@@ -92,36 +104,37 @@ const parseQueryString = (res, _query, curModel, incModels) => {
   if (whereTmp) {
     // Parsing where
     for (let i = 0; i < whereTmp.length; i += 1) {
-      const split = whereTmp[i].split("=").join(".").split(".");
-      if (split.length === 2) {
-        if (curColumns.indexOf(split[0].trim()) === -1) {
+      if (whereTmp[i].length === 2) {
+        if (curColumns.indexOf(whereTmp[i][0].trim()) === -1) {
           return {
             error: 1,
             code: "SEQUELIZE_UNKNOWN_WHERE",
-            message: `${split[0].trim()}=${split[1]}`,
+            message: `${whereTmp[i][0].trim()}=${whereTmp[i][1]}`,
           };
         }
-        where[split[0].trim()] = split[1];
+        where[whereTmp[i][0].trim()] = whereTmp[i][1];
       }
       // include의 where 처리
       else {
         // {객체명}.{속성명} 일 경우
         for (let j = 0; j < include.length; j += 1) {
-          if (include[j].model === incModels[split[0].trim()]) {
+          if (include[j].model === incModels[whereTmp[i][0].trim()]) {
             // model이 {객체명}과 일치하는 include를 찾음
             if (
               Object.keys(include[j].model.rawAttributes).indexOf(
-                split[1].trim()
+                whereTmp[i][1].trim()
               ) === -1
             ) {
               return {
                 error: 1,
                 code: "SEQUELIZE_UNKNOWN_WHERE",
-                message: `${split[0].trim()}.${split[1].trim()}=${split[2]}`,
+                message: `${whereTmp[i][0].trim()}.${whereTmp[i][1].trim()}=${
+                  whereTmp[i][2]
+                }`,
               };
             }
             if (!include[j].where) include[j].where = {}; // 초기화
-            include[j].where[split[1].trim()] = split[2]; // 찾은 include의 attributes에 항목 추가
+            include[j].where[whereTmp[i][1].trim()] = whereTmp[i][2]; // 찾은 include의 attributes에 항목 추가
           }
         }
       }
