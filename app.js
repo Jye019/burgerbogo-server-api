0239236b673e4388b677aadabecb18f8d31267b5
minxd95 <newobj95@gmail.com>
2020-09-26 17:45:32 +0900

    Bug: 오류 수정

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import auth from './routes/auth';
-import burger from './routes/burger';
-import brand from './routes/brand';
-import review from './routes/review';
-import filter from './routes/filter';
-import ingredient from './routes/ingredient';
-import models from './models';
-import db from './library/db';
-import { logger } from './library/log';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import auth from "./routes/auth";
+import burger from "./routes/burger";
+import brand from "./routes/brand";
+import review from "./routes/review";
+import filter from "./routes/filter";
+import ingredient from "./routes/ingredient";
+import models from "./models";
+import db from "./library/db";
+import { logger } from "./library/log";
 
-<<<<<<< HEAD
-// jhkim_test2qweiruqwiruoewqiruewiofhjkasfhlkasnfljcmldszncmrnjlkasnfcgjkrszlrcls
-// jhkim_test2qweiruqwiruoewqiruewiofhjkasfhlkasnfljcmldszncmrnjlkasnfcgjkrszlrcls
-// jhkim_test2qweiruqwiruoewqiruewiofhjkasfhlkasnfljcmldszncmrnjlkasnfcgjkrszlrcls
-=======
-// jhkim_test2 정연진~@@#ㅃ@$#ㅃㄸㅉㅁㄸㅇㅁ
->>>>>>> Etc: lhh rebgase test
 // 테이블이 존재하지 않으면 테이블 생성
 db.syncronize(models);
 
@@ -28,22 +21,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.use('/auth', auth);
-app.use('/burger', burger);
-app.use('/brand', brand);
-app.use('/review', review);
-app.use('/filter', filter);
-app.use('/ingredient', ingredient);
+app.use("/auth", auth);
+app.use("/burger", burger);
+app.use("/brand", brand);
+app.use("/review", review);
+app.use("/filter", filter);
+app.use("/ingredient", ingredient);
 
 // env 설정
 dotenv.config();
 
 // 에러 핸들러
 app.use((req, res) => {
-  logger.error('404 Not Found');
-  res.status(404).json({ message: '404 Not Found' });
+  logger.error("404 Not Found");
+  res.status(404).json({ message: "404 Not Found" });
 });
 
 app.listen(3000, () => {
-  console.log('Listening at port 3000');
+  console.log("Listening at port 3000");
 });
