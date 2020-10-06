b34e8f3c47d6b119ab1f3949fa50fd2c6e955ca9
Jye019 <jihyekim019@gmail.com>
2020-10-06 23:46:27 +0900

    Feat: 리턴 형식 변경

--- routes/filter.js
+++ routes/filter.js
@@ -11,17 +11,17 @@ router.get('/', async(req, res) => {
 
         const list =  await db.sequelize.query(
                         `SELECT DISTINCT image, 
-                                score, 
-                                score_cnt,
                                 price_single, 
                                 price_set, 
                                 price_combo,
-                                brand_id,
                                 released_at,
                                 burger.id as id,
-                                burger.name as name, 
-                                (select name from brands where id = brand_id) as brand_name
-                        FROM burgers AS burger
+                                score, 
+                                score_count,
+                                burger.name as name,` +
+                                'brand_id as `Brand.id`,' +  
+                                '(select name from brands where id = brand_id) as `Brand.name`'+
+                        `FROM burgers AS burger
                         LEFT JOIN (
                             SELECT burger_id, 
                                    id 
@@ -36,13 +36,13 @@ router.get('/', async(req, res) => {
                         ON burger.id = bi2.burger_id
                         LEFT JOIN (
                             SELECT ROUND(AVG(SCORE), 1) AS score,
-                                    COUNT(burger_id) AS score_cnt,
-                                    burger_id 
+                                   COUNT(burger_id) AS score_count,
+                                   burger_id 
                             FROM reviews 
                             GROUP BY burger_id) AS review 
                         ON review.burger_id=burger.id
                         WHERE bi.id IS NULL
-                        AND COALESCE(score_cnt, 0) >= ${(order==='score')? 3: 0}
+                        AND COALESCE(score_count, 0) >= ${(order==='score')? 3: 0}
                         AND ( fn_search_csnt(name) like '%${keyword || ''}%' OR name like '%${keyword || ''}%' )
                         ${ (brand)? `AND brand_id IN (${brand})` : ''}
                         ORDER BY ${order} ${(order==='score')? 'DESC' : 'ASC'}`,
