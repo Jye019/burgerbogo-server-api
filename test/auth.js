686a59ef5bd80fbef8ce09c08d8c442d5f726064
Jye019 <jihyekim019@gmail.com>
2020-10-12 15:56:59 +0900

    Feat: 회원가입 테스트 코드 작성 (url: ec2 / database: dev) -> 변경 필

--- test/auth.js
+++ test/auth.js
@@ -1,6 +1,5 @@
 import chai from 'chai';
 import chailHttp from 'chai-http';
-import app from '../app';
 
 const url = 'ec2-54-180-86-187.ap-northeast-2.compute.amazonaws.com:3000';
 const expect = chai.expect;
@@ -14,13 +13,15 @@ describe('POST /auth/join', () => {
     it('it should be registered', done => {
         chai.request(url)
             .post('/auth/join')
-            .set('content-type', 'application/json; charset=utf-8')
-            .send({email:'jihyekim019@gmail.com',
+            .set('content-type', 'application/json')
+            .send({email:'wookkya2@gmail.com',
                    password: 'shTmfah1'})
             .end((err, res) => {
                 if(err) 
                     done(err);
+                expect(JSON.parse(res.text).code).to.equal('AUTH_SUCCESS')
                 expect(res.status).to.equal(200)
+                done();
              });
     })
 });
