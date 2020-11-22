8c5639f57cb93b8daa23f974a6049e9b95fa16e8
Jye019 <jihyekim019@gmail.com>
2020-11-23 03:07:18 +0900

    Feat: 정렬 방법 변경

--- routes/filter.js
+++ routes/filter.js
@@ -79,8 +79,8 @@ router.get('/', async(req, res) => {
                         AND COALESCE(score_count, 0) >= ${(order==='score')? 3: 0}
                         AND ( fn_search_csnt(name) like '%${keyword || ''}%' OR replace(name, ' ', '') like '%${keyword || ''}%' )
                         ${ (brand)? `AND brand_id IN (${brand})` : ''}
-                        ORDER BY ${order} ${(order==='score')? 'DESC' : 'ASC'}
-                        ${(order==='released_at_year')? ', released_at_month ASC, released_at_day ASC' : ''}`,
+                        ORDER BY ${order} ${(order==='name')? 'DESC' : 'ASC'}
+                        ${(order==='released_at_year')? ', released_at_month DESC, released_at_day DESC' : ''}`,
                         { 
                             type: QueryTypes.SELECT,
                             nest: true,
